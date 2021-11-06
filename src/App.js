28fa07d17bbd71909477d600b222e47793e6e71e
Richard Bankole <rickoslyder@gmail.com>
2021-11-06 14:10:05 +0000

    Axios test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import nftLogo from './assets/chaos.png';
 import cbTitleLogo from './assets/huey-gun-logo.png';
 import './App.css';
 import { ethers } from 'ethers';
+import Axios from 'axios';
 
 import cryptoBoondocks from './utils/CryptoBoondocks.json'
 import SelectCharacter from './Components/SelectCharacter';
@@ -140,8 +141,42 @@ const App = () => {
     if (currentAccount) {
       console.log('Current Account:', currentAccount);
       fetchNFTMetadata();
+      generateCompany();
     }
   }, [currentAccount]);
+
+  let to = []
+  const getCompanyName = async () => {
+    let name_data = await Axios.get("https://random-data-api.com/api/company/random_company");
+    let cName = name_data.data.business_name
+    return cName;}
+
+  const generateCompany = async () => {for (let i=0; i < 20; i++) {
+    function randInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+
+    let cName = await getCompanyName()
+    let startVal = randInt(1990, 2003);
+    let endVal = startVal + randInt(1, 18)
+
+    if (endVal > 2021) {
+      endVal = 2021
+    } 
+    
+    let company = {name: cName, start: startVal.toString(), end: endVal.toString()}
+
+    to = [...to, company]
+}
+
+let failures = to.filter(company => (company.end - company.start <= 3))
+    console.table(to)
+    console.log("%c Failures:", "color: orange; font-weight:bold;", failures)
+
+}
+
 
 
   return (
