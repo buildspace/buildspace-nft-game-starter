2cab3e4c9fde0da0def3b81bc0b18246dc83f419
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2021-12-21 10:59:47 +0530

    feat (app) : connect wallet 🔌
    - check is walllet is already connected
    - add button that triggers connect wallet action

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
@@ -7,6 +7,67 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  // Checkin if wallet is connected or not
+  const checkIfWalletIsConnected = async() => {
+    try{
+      // First make sure we have access to window.ethereum
+      // MetaMask automatically injects an special object named ethereum
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      // Check if user is connected to rinkeby test network
+      let chainId = await ethereum.request({ method: 'eth_chainId' });
+      console.log("Connected to chain " + chainId);
+
+      // String, hex code of the chainId of the Rinkebey test network
+      const rinkebyChainId = "0x4"; 
+      if (chainId !== rinkebyChainId) {
+        alert("You are not connected to the Rinkeby Test Network!");
+      }
+
+      // Checking if we're authorized to acess the user's wallet
+      const accounts = await ethereum.request({ method: 'eth_accounts'});
+      if (accounts.length !== 0){
+        const account = accounts[0];
+        console.log("Found an authorized account: ", account);
+      }else{
+        console.log("No authorized account found!");
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  //Connects my wallet to this site
+  const connectWallet = async() => {
+    try{
+      // Looking for ethereum object
+      const {ethereum} = window;
+      if (!ethereum) {
+        alert("Get MetaMask");
+        return;
+      }
+      // If we find MetaMask, ask MetaMask to give access to user's wallet
+      const accounts = await ethereum.request({method: "eth_requestAccounts"});
+      console.log("Connected : ",accounts[0]);
+      setCurrentAccount(accounts[0]);
+    }catch(error){
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
@@ -18,6 +79,13 @@ const App = () => {
               src="https://i.imgur.com/d4VT4dn.gif"
               alt="Hora Hora Gif"
             />
+            {/* Button that trigger wallet connect */}
+            <button
+              className="cta-button connect-wallet-button"
+              onClick={connectWallet}
+            >
+              Connect Wallet To Play
+            </button>
           </div>
         </div>
         <div className="footer-container">
