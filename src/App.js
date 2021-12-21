a3c898cdf641978058c011bb90fa1731446d4052
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2021-12-21 10:36:11 +0530

    feat : initial commit 🎉

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
-          <p className="sub-text">Team up to protect the Metaverse!</p>
+          <p className="header gradient-text">⚔️ Metaverse Sorcerers ⚔️</p>
+          <p className="sub-text">Team up to protect the Metaverse from Cursed Spirits!</p>
           <div className="connect-wallet-container">
             <img
-              src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
-              alt="Monty Python Gif"
+              src="https://i.imgur.com/d4VT4dn.gif"
+              alt="Hora Hora Gif"
             />
           </div>
         </div>
